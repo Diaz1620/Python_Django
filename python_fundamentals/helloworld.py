810a77ba26858197ae3b09b47cd7bc157cb211ab
Diaz1620 <77040700+Diaz1620@users.noreply.github.com>
2021-07-17 11:16:46 -0400

    first commit

---
+++ python_fundamentals/helloworld.py
@@ -0,0 +1,11 @@
+print("what it doo everyone!")
+
+name = "Lebron"
+age = 35
+
+# print(name + " is the goat at age " + str(age))
+
+print(f"{name} is the goat at age {age}")
+
+# print("{} is the goat at age {}".format(name, age))
+
