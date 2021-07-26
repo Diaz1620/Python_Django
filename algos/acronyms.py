8325afd35275288c1f4fce1ed5d0635216917912
Diaz1620 <77040700+Diaz1620@users.noreply.github.com>
2021-07-25 22:44:20 -0400

    made a change to acronym.py

--- algos/acronyms.py
+++ algos/acronyms.py
@@ -1,12 +1,12 @@
 def generateAcronym(stringInput):
     acronym = ""
     if stringInput[0] != " ":
-        acronym += stringInput[0].upper()
+        acronym += stringInput[0]
     for i in range(len(stringInput)):
         if stringInput[i-1] == " " and stringInput[i] != " ":
-            acronym += stringInput[i].upper()
-    print(acronym)
-    return acronym
+            acronym += stringInput[i]
+    print(acronym.upper())
+    return acronym.upper()
 
 
 generateAcronym("        national      basketball        association")
