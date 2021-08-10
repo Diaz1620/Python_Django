1d653c0a42ce9f62581d7b1958f104174bc2ed91
Diaz1620 <77040700+Diaz1620@users.noreply.github.com>
2021-08-09 22:09:48 -0400

    added code

--- OOP/bankingApp.py
+++ OOP/bankingApp.py
@@ -6,6 +6,10 @@ class User:
     # adding deposit method
     def make_deposit(self, amount):
         self.account_balance += amount
+    def make_withdrawal(self,amount):
+        self.account_balance -= amount
+    def display_user_balance(self):
+        print(f"{self.name}, Balance: {self.account_balance}")
 
 guido = User("Guido van Rossum", "guido@python.com")
 monty = User("Monty Python", "monty@python.com")
@@ -15,4 +19,7 @@ guido.make_deposit(200)
 monty.make_deposit(50)
 print(guido.account_balance)
 print(monty.account_balance)
+guido.make_withdrawal(50)
+guido.display_user_balance
+
 
