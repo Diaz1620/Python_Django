95ea2dc630c27c6068da408328905dd874e4841a
Diaz1620 <77040700+Diaz1620@users.noreply.github.com>
2021-08-09 22:32:00 -0400

    added lines

--- OOP/bankingApp.py
+++ OOP/bankingApp.py
@@ -9,7 +9,12 @@ class User:
     def make_withdrawal(self,amount):
         self.account_balance -= amount
     def display_user_balance(self):
-        print(f"{self.name}, Balance: {self.account_balance}")
+        print(self.name + ", Balance: $" + str(self.account_balance) )
+    def transfer(self, other_person, amount):
+        self.make_withdrawal(amount)
+        other_person.account_balance += amount
+        self.display_user_balance
+        other_person.display_user_balance
 
 guido = User("Guido van Rossum", "guido@python.com")
 monty = User("Monty Python", "monty@python.com")
@@ -20,6 +25,16 @@ monty.make_deposit(50)
 print(guido.account_balance)
 print(monty.account_balance)
 guido.make_withdrawal(50)
-guido.display_user_balance
+guido.display_user_balance()
 
 
+larry = User("Larry Larrod", "ll@test.com")
+harry = User("Harry Potts", "harryp@wiz.com")
+
+larry.make_deposit(100)
+larry.make_deposit(300)
+larry.make_deposit(200)
+
+larry.display_user_balance()
+
+larry.transfer(harry, 200)
