c089f0ef7c3a100747d298c94bdbb638910f351c
Diaz1620 <77040700+Diaz1620@users.noreply.github.com>
2021-08-10 19:07:16 -0400

    chained methods on bankingapp.py

--- OOP/bankingApp.py
+++ OOP/bankingApp.py
@@ -19,22 +19,15 @@ class User:
 guido = User("Guido van Rossum", "guido@python.com")
 monty = User("Monty Python", "monty@python.com")
 
-guido.make_deposit(100)
-guido.make_deposit(200)
+guido.make_deposit(100).make_deposit(200)
 monty.make_deposit(50)
 print(guido.account_balance)
 print(monty.account_balance)
-guido.make_withdrawal(50)
-guido.display_user_balance()
+guido.make_withdrawal(50).display_user_balance()
 
 
 larry = User("Larry Larrod", "ll@test.com")
 harry = User("Harry Potts", "harryp@wiz.com")
 
-larry.make_deposit(100)
-larry.make_deposit(300)
-larry.make_deposit(200)
+larry.make_deposit(100).make_deposit(300).make_deposit(200).display_user_balance().transfer(harry, 200)
 
-larry.display_user_balance()
-
-larry.transfer(harry, 200)
