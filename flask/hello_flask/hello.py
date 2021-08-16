32249ebbc67218909ab66ebfc75c0bc118dae6ec
Diaz1620 <77040700+Diaz1620@users.noreply.github.com>
2021-08-16 17:51:33 -0400

    added now logic to hello.py

--- flask/hello_flask/hello.py
+++ flask/hello_flask/hello.py
@@ -37,6 +37,16 @@ def showAPlayer(playerId):
 def playboxes(number,color):
     return render_template('boxes.html', num = number, color = color)
 
+@app.route('/lists')
+def render_lists():
+    # Soon enough, we'll get data from a database, but for now, we're hard coding data
+    student_info = [
+        {'name' : 'Michael', 'age' : 35},
+        {'name' : 'John', 'age' : 30 },
+        {'name' : 'Mark', 'age' : 25},
+        {'name' : 'KB', 'age' : 27}
+    ]
+    return render_template("lists.html", random_numbers = [3,1,5], students = student_info)
 
 
 
